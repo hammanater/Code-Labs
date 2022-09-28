b4eb25064ed4c8ec16719f299d409214d673f63f
hammanater <hammanbraden24@g-apps.monett.k12.mo.us>
2022-09-27 19:16:59 -0500

    Did accordian and got all the way through exercise 8

---
+++ Class10/6.js
@@ -0,0 +1,25 @@
+let firstName;
+firstName = "John";
+console.log(firstName)
+
+let lastName
+lastName = "Bagebiss"
+console.log(lastName)
+
+//let favoriteNumber
+//favoriteNumber = "267"
+//console.log(favoriteNumber)
+
+let age
+age = "22"
+
+let John
+John = firstName
+John = lastName
+John = age
+console.log(John)
+
+let user = {
+    email: "John123@gmail.com",
+    username: "john123",
+  };
